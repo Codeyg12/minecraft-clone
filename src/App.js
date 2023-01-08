9e70a479412e9e0548fba87784e3b757fde4ba25
Codeyg12 <codey.gallup@gmail.com>
2023-01-07 17:35:07 -0700

    added textures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { dirtImg } from './images'
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
-    <div className="App">
-      <img src={dirtImg}></img>
-    </div>
+    <>
+    <Canvas>
+      <Sky sunPosition={[100, 100, 20]} />
+    </Canvas>
+    </>
   );
 }
 
