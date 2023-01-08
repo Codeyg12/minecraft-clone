7d7aec47e25acf1622e63471e4c85d7091bb71df
Codeyg12 <codey.gallup@gmail.com>
2023-01-07 17:25:27 -0700

    added all needed dependencies and deleted extra files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { dirtImg } from './images'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={dirtImg}></img>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+import dirtImg from './dirt.jpg'
+import glassImg from './glass.png'
+import woodImg from './wood.png'
+import logImg from './log.jpg'
+import grassImg from './grass.jpg'
+
+export { dirtImg, glassImg, woodImg, logImg, grassImg }
