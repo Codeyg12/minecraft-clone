3aa1d4f99102edbf4fbc52ce53d0e669bc2b1b81
Codeyg12 <codey.gallup@gmail.com>
2023-01-07 20:08:04 -0700

    adding and removing cubes works

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -8,7 +8,7 @@ export const Cube = ({ position, texture }) => {
     position,
   }));
 
-  const [addCube, removeCube] = useStore((state) => [state.addCube])
+  const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube])
 
 
   const activeTexture = textures[texture + "Texture"];
@@ -17,22 +17,28 @@ export const Cube = ({ position, texture }) => {
     <mesh onClick={(e) => {
       e.stopPropagation()
       const clickedFace = Math.floor(e.faceIndex / 2)
-      console.log(clickedFace)
       const { x, y, z } = ref.current.position
       if (e.altKey) {
         removeCube(x, y, z) 
+        return
       } else if (clickedFace === 0) {
         addCube(x + 1, y, z)
+        return
       } else if (clickedFace === 1) {
         addCube(x - 1, y, z)
+        return
      } else if (clickedFace === 2) {
         addCube(x, y + 1, z)
+        return
      } else if (clickedFace === 3) {
         addCube(x, y - 1, z)
+        return
      } else if (clickedFace === 4) {
       addCube(x, y, z + 1)
+      return
      } else if (clickedFace === 5) {
       addCube(x, y, z - 1)
+      return
      }
     }} ref={ref}>
       <boxGeometry attach="geometry" />
